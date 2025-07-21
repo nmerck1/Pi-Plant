0deccdaa119077395c7f535248b7030cc3119609
Nathaniel <nathanielmerck@yahoo.com>
2025-07-21 16:10:46 -0400

    Added configurations

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,8 +17,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Serve static files from wwwroot
+app.UseStaticFiles();
+
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Fallback to React for client-side routing
+app.MapFallbackToFile("index.html");
 
 app.Run();
